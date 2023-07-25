dd46ea060c67c2c049f18b3a6958b17110a54ad1
Affan Khan <kaffan022@gmail.com>
2023-07-25 15:20:37 +0530

    New commit on 25th july 2023 at 3:20 PM

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Logo from './Components/SideBar/Logo';
 import SideBar from './Components/SideBar/SideBar';
 import * as ReactDOM from 'react-dom';
 import AddNewBoard from './Components/Boards/AddNewBoard';
+import NewTask from './Components/Tasks/NewTask';
 
 function App() {
   return (
     <div className="App">
       {ReactDOM.createPortal(<AddNewBoard></AddNewBoard>, document.getElementById('portal1'))}
+      {ReactDOM.createPortal(<NewTask></NewTask>, document.getElementById('portal2'))}
       <MainPage></MainPage>
     </div>
   );
--- src/Components/Tasks/NewTask.jsx
+++ src/Components/Tasks/NewTask.jsx
@@ -1,30 +1,88 @@
-import { Grid } from "@mui/material";
+import { Button, Grid } from "@mui/material";
 import { Fragment } from "react";
 
 const NewTask = () =>{
     return(
         <Fragment>
-            <div>
-                <Grid container>
-                    <Grid item>
+            <div style={{
+                position: "absolute",
+                width: "100%",
+                height: "100vh",
+                zIndex:"3",
+                backgroundColor:"rgba(0,0,0,0.6)",
+                backdropFilter:"blur(1px)",
+            }}>
+                <Grid container direction="column" spacing={1} sx={{
+                    position: "absolute",
+                    left: "35%",
+                    top: "7%",
+                    zIndex: "1",
+                    margin: "15px",
+                    padding: "15px",
+                    width: "35%",
+                    height:"fit-content",
+                    backdropFilter: "blur(5px)",
+                    backgroundColor: "rgba(255, 255, 255, 1)",
+                    textAlign: "left",
+                }}>
+                    <Grid item sx={{
+                        fontWeight:"700"
+                    }}>
                         <span>Add New Task</span>
                     </Grid>
                     <Grid item>
-                        <label>Title</label>
-                        <input type="text" placeholder="e.g. Take coffee break"></input>
+                        <label style={{
+                            padding:"5px 0",
+                            margin:"5px 0",
+                            color:"lightgray",
+                            fontWeight:"700"
+                        }}>Title</label><br />
+                        <input type="text" placeholder="e.g. Take coffee break" style={{
+                             padding:"10px",
+                             margin:"10px 0",
+                             width:"90%",
+                             border:"solid 0.6px",
+                             borderColor:"lightgrey",
+                             borderRadius:"7px"
+                        }}></input>
                     </Grid>
                     <Grid item>
-                        <label>Description</label>
-                        <input type="text"></input>
+                        <label style={{
+                            padding:"5px 0",
+                            margin:"5px 0",
+                            color:"lightgray",
+                            fontWeight:"700"
+                        }}>Description</label><br />
+                        <textarea cols="80" rows="3" placeholder="e.g. it's always good to take a break. This 15 mins break will recharge the batteries a little.... " style={{
+                            padding:"10px",
+                             margin:"10px 0",
+                             width:"90%",
+                             height:"100px",
+                             border:"solid 0.6px",
+                             borderColor:"lightgrey",
+                             borderRadius:"7px"
+                        }}></textarea>
                     </Grid>
-                    <Grid>
+                    <Grid item>
+                        <label sx={{
+                            padding:"3px 0",
+                            margin:"3px 0",
+                            color:"lightgray",
+                            fontWeight:"700"
+                        }}>Subtasks</label>
+                        <Grid container>
 
+                        </Grid>
+                        <Button>+ Add New Subtask</Button>
                     </Grid>
-                    <Grid>
-
+                    <Grid item>
+                        <label>Status</label><br />
+                        <select name="status" id="" disabled="disabled">
+                            <option value="Todo">To Do</option>
+                        </select>
                     </Grid>
-                    <Grid>
-
+                    <Grid item>
+                        <Button>Create New Task</Button>
                     </Grid>
                 </Grid>
             </div>
