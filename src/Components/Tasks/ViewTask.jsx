83e5f28539f0534172b27bb93d94c7b9c25b37d4
Affan Khan <kaffan022@gmail.com>
2023-08-07 20:27:19 +0530

    New commit on 7th august 2023 at 8:27 PM

--- src/Components/Tasks/ViewTask.jsx
+++ src/Components/Tasks/ViewTask.jsx
@@ -22,7 +22,7 @@ const ViewTask = ({col, Task, CurrentBoard}) =>{
                 zIndex: "1",
                 margin: "15px",
                 padding: "25px",
-                width: "35%",
+                width: "30%",
                 height: "fit-content",
                 backdropFilter: "blur(5px)",
                 backgroundColor: "rgba(255, 255, 255, 1)",
@@ -32,7 +32,10 @@ const ViewTask = ({col, Task, CurrentBoard}) =>{
                     fontWeight: "700"
                 }}>
                     <Grid container sx={{
-
+                        padding: "5px 0",
+                        margin: "5px 0",
+                        color: "#000000",
+                        fontWeight: "700"
                     }}>
                         <Grid item sx={{
                             width:"80%"
@@ -44,18 +47,41 @@ const ViewTask = ({col, Task, CurrentBoard}) =>{
                         </Grid>
                     </Grid>
                 </Grid>
-                <Grid item>
-                    {Task.decription}
+                <Grid item sx={{
+                     padding: "5px 0",
+                     margin: "5px 0",
+                     color:"rgb(130, 143, 163)",
+                     width:"100%",
+                     height:"auto",
+                     fontSize:"16px"
+                }}>
+                    <div style={{width:"100%",height:"auto",wordBreak: "break-word",lineHeight:"21px"}}>
+                    {Task.description}
+                    </div>
                 </Grid>
                 <Grid item>
-                    <Grid container>
-                        <Grid item>
-                            <label>Subtasks</label>
+                    <Grid direction="column" container>
+                        <Grid item sx={{
+                            padding: "5px 0",
+                            margin: "5px 0",
+                        }}>
+                            <label style={{
+                                color:"mediumgrey",
+                                fontWeight:"700"
+                            }}>Subtasks</label>
                         </Grid>
                         {Task.subtasks.length!==0 && Task.subtasks.map((ele,i)=>(
-                            <Grid item>
-                                <span><input type="checkbox" id={i} /></span>
-                                <label>{ele}</label>
+                            <Grid  item sx={{
+                            padding: "7px 5px",
+                            margin: "5px 0",
+                            width:"100%",
+                            backgroundColor:"#f4f7fd",
+                            fontSize:"16px",
+                            color: "rgb(130, 143, 163)",
+                            fontWeight:"600"
+                            }}>
+                                <input style={{padding:"5px"}} type="checkbox" id={i} />
+                                <label style={{padding:"5px"}}>{ele}</label>
                             </Grid>
                         ))}
                     </Grid>
