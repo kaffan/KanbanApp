bcfb7e16e7e4189fad76b3bc8f87984bbf21ab8d
Affan Khan <kaffan022@gmail.com>
2023-09-22 22:27:50 +0530

    New Commit at 10:25 PM on 22 Sept 2023

--- src/Components/Boards/DisplayBoards.jsx
+++ src/Components/Boards/DisplayBoards.jsx
@@ -6,13 +6,14 @@ import ColumnDisplay from "../Columns/ColumnDisplay";
 // import CurrentBoard from "../../Reducers/CurrentBoard";
 
 const DisplayBoards = () =>{
-    const state = useSelector((state)=>state.Boards.find((ele)=>ele.clicked));
-    const globalState = useSelector((state)=>state);
-    console.log(globalState);
+    const state = useSelector((state)=>state);
+    const CurrentBoard = useSelector((state)=>state.Boards.find((ele)=>ele.clicked));
+    const columns = [...state.Columns].filter((ele)=>ele.boardName===CurrentBoard.name);
+    // console.log(globalState);
     return(
         <Fragment>
             {/* <Header Element={state}></Header> */}
-            {state.columns && (state.columns.length!==0) ? <ColumnDisplay CurrentBoard={state} columns={state.columns}></ColumnDisplay> : <NoColumns></NoColumns>}
+            {columns && (columns.length!==0) ? <ColumnDisplay CurrentBoard={state} columns={columns}></ColumnDisplay> : <NoColumns></NoColumns>}
         </Fragment>
     )
 }
